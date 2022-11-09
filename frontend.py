87a66345ca5b49a68703ccc2267b44466305321d
Harikrishnan D <harideegee@gmail.com>
2022-11-09 11:20:25 +0530

    final changes made (model working)

--- frontend.py
+++ frontend.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, jsonify, url_for 
+import pickle
+import numpy as np
+import pandas as pd 
 
+model = pickle.load(open('rfc.pkl', 'rb'))
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '78fabf5575579bcb76daea4131c8b06f8d41462b08850e3f'
 
@@ -10,6 +14,7 @@ def hello_world():
 @app.route("/about/")
 def about_page():
     return render_template("about.html")
+
 
 @app.route("/result/", methods=['POST', 'GET'])
 def potability_calc():
@@ -23,14 +28,20 @@ def potability_calc():
         oc = request.form["oc"]
         thm = request.form["trihalomethanes"]
         turb = request.form["turbidity"]
+        data = {"ph": ph, "hard": hard, "solid": solid, "chamine": chamine, "sulfate": sulfate, "cond": cond, "oc": oc, "thm": thm, "turb": turb}
+        new_data = np.array(list(data.values())).reshape(1,-1)
+        output = model.predict(new_data)
+        output = output[0]
+        if output == 1:
+            output = "Yes"
+        else:
+            output = "No"
         
-        context = {"ph": ph, "hard": hard, "solid": solid, "chamine": chamine, "sulfate": sulfate, "cond": cond, "oc": oc, "thm": thm, "turb": turb}
+        context = {"ph": ph, "hard": hard, "solid": solid, "chamine": chamine, "sulfate": sulfate, "cond": cond, "oc": oc, "thm": thm, "turb": turb, "potability": output}
 
         return render_template("results.html", context=context)
     else:
         return redirect("/")
-        
-        
 
 
 if  __name__ == "__main__":
